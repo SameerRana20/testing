f7413347583c7c2ab1c062d82aa8d7c0e4b712a7
Sameer Rana <sameerrana20300@gmail.com>
2025-12-24 20:27:32 +0530

    add route

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const express = require("express")
 const app = express()
 const port = 3000
 
+const data= {"name": "sameer rana",
+    "id": 8017
+}
+
 app.get("/" , (req, res)=>{
     res.send("hello world ji")
 })
+
+app.get ("/me", (req ,res)=>{
+    res.json(data);
+})
+
+
 
 app.listen(process.env.PORT, ()=>{console.log("started")})
